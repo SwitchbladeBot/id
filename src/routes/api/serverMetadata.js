548fd04f47f3df921f7b300d5b4933a646b86008
Pedro Fracassi <pedroluizfracassi@gmail.com>
2020-10-22 18:31:15 +0000

    Initial rework stuff

--- src/routes/api/serverMetadata.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({router}) => {
-  /**
-   * Authorization Server Metadata Endpoint (RFC 8414 https://tools.ietf.org/html/rfc8414)
-   * This can contain a signed_metadata field containing the same information but signed as a JSON Web Token (See Section 2.1 of RFC 8114)
-   */
-  router.get('/server_metadata', (req, res) => {
-    res.send('TODO')
-  })
-}
--- src/routes/api/tokenInformation.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({router}) => {
-  /**
-   * Token Introspection Endpoint (RFC 7662 https://tools.ietf.org/html/rfc7662)
-   */
-  router.get('/token_information', (req, res) => {
-    res.send('TODO')
-  })
-}
