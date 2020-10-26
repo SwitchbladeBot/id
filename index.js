46306811906114551657d4eaedaf1c8c963ef525
pedrofracassi <pedroluizfracassi@gmail.com>
2020-10-25 21:29:34 -0300

    Add gitignore

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
